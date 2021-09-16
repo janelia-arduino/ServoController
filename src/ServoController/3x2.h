44f62c88a34c6d1d1dec5d3adbaa03770453905e
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 14:40:16 -0400

    Update email address

--- src/ServoController/3x2.h
+++ src/ServoController/3x2.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef SERVO_CONTROLLER_3X2_CONSTANTS_H
 #define SERVO_CONTROLLER_3X2_CONSTANTS_H
